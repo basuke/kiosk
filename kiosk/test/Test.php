98ef1bb1b657c49d49fedecd887127ab06c626f5
Yosuke Basuke Suzuki <basuke@kanshin.com>
2011-01-17 19:33:06 +0900

    WebTesterを追加

--- kiosk/test/Test.php
+++ kiosk/test/Test.php
@@ -1,8 +1,13 @@
 <?php
 
 class Kiosk_Test_Test {
+	var $_simpletest_path = null;
+	
 	function setupSimpletest($path) {
-		require_once $path. '/autorun.php';
+		$this->_simpletest_path = $path;
+		
+		require_once $this->_simpletest_path. '/autorun.php';
+		require_once $this->_simpletest_path. '/web_tester.php';
 		require_once KIOSK_LIB_DIR. '/test/TextReporter.php';
 	}
 }
